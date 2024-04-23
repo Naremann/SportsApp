2993d5c32bba343f127e49350c3809051d386d59
Naremann <naremansharkan5@gmail.com>
2024-04-23 16:42:09 -0700

    create home screen (contains 4 sports cells), create custom collectionView cell by nib file

--- SportsApp/ViewController.swift
+++ SportsApp/ViewController.swift
@@ -7,12 +7,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    var sports : [SportItem]?
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sports = getSportsData()
+        registerCustomCell()
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sports!.count
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func getSportsData() -> [SportItem]{
+        let sports =  [SportItem(sportImage: "sports",sportTitle: "Football"),
+                       SportItem(sportImage: "sports",sportTitle: "BasketBall"),
+                       SportItem(sportImage: "sports",sportTitle: "Cricket"),
+                       SportItem(sportImage: "sports",sportTitle: "Tennis")]
+        return sports
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SportsCollectionViewCell
+        cell.sport_name_lb.text = sports?[indexPath.row].sportTitle
+               cell.sport_img.image = UIImage(named: (sports?[indexPath.row].sportImage)!)
+        
+        return cell
+    }
+   
+    
+ 
+    
+    
+    
+    
+
+        
+
+  
+    
+    
+   
+    func registerCustomCell(){
+        let cellNib = UINib(nibName: "SportsCollectionViewCell", bundle: nil)
+      
+                
+      
+        collectionView.register(cellNib, forCellWithReuseIdentifier: "cell")
+
+         
+       
+       collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.frame = view.bounds
+      
+        
+
+        
+        
+
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected")
+    }
+    
+   
 
 
 }
